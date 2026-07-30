d6a1e5ea8d869ed77afeb301c8e11d7905b9df3e
Badr <khalloukibadr6@gmail.com>
2026-07-30 13:00:31 +0100

    Initial commit : Création de la structure globale et du Backend

---
+++ helpdeskBackend/src/test/java/ma/helpdeskBackend/HelpdeskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.helpdeskBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
